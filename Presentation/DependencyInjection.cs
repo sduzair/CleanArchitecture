be78ba24612b226b0db8052249ffa672fbf05956
sduzair <sduzairahmed@gmail.com>
2023-03-17 23:40:36 -0400

    presentation application builder extension

--- Presentation/DependencyInjection.cs
+++ Presentation/DependencyInjection.cs
@@ -1,11 +1,42 @@
 ﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Presentation;
 public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddProblemDetails();
         return services;
+    }
+}
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UsePresentation(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler();  //for unhandled exceptions in production
+            app.UseHsts();
+        }
+
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseExceptionHandler("/Error");
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+        return app;
     }
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -4,8 +4,6 @@ using Infrastructure;
 
 using Presentation;
 
-using WebApp;
-
 internal sealed class Program
 {
     private static void Main(string[] args)
@@ -13,33 +11,15 @@ internal sealed class Program
         var builder = WebApplication.CreateBuilder(args);
 
         {
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddInfrastructure(builder.Configuration)
+            builder.Services.AddPresentation()
+                .AddInfrastructure(builder.Configuration)
                 .AddApplication()
                 .AddPresentation();
-            builder.Services.AddProblemDetails();
         }
 
         var app = builder.Build();
 
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-        app.MapControllers();
-
+        app.UsePresentation(app.Environment);
         app.Run();
     }
 }
